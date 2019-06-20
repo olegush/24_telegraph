cf3f5bce07489854c1e56b1ac184880ff1dc74e5
olegush <oleg107117@gmail.com>
2019-06-20 12:38:19 +0300

    bugs fixed according code review

--- server.py
+++ server.py
@@ -1,126 +1,124 @@
 import os
 import re
 import json
-from hashlib import blake2b
-from hmac import compare_digest
-from urllib.parse import urlsplit
+import uuid
 
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, make_response, send_from_directory
 
 
-ARTICLES_DIR = 'articles'
-ARTICLES_EXT = '.txt'
 MAX_SLUG_LENGTH = 100
-SLUG_CHARS = {
+SLUG_TRANS_MAP = {
     'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e',
     'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm',
     'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
     'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch',
     'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'y', 'я': 'ya'}
-SECRET_KEY = b'xk&$%h4788*^&'
-AUTH_SIZE = 16
 COOKIE_AGE = 60*60*24*30
 ERRORS = {
-    'empty_field': 'You must fill in all of the fields',
+    'empty_header': 'You must fill in header at least.',
 }
 
 app = Flask(__name__)
 
 
-def sign(cookie):
-    h = blake2b(digest_size=AUTH_SIZE, key=SECRET_KEY)
-    h.update(cookie.encode('utf-8'))
-    return h.hexdigest()
-
-
-def verify(cookie, sig):
-    good_sig = sign(cookie)
-    return bool(sig) and compare_digest(good_sig, sig)
-
-
-def get_slug_and_filepath(header, url):
-    # If article editing
-    if url:
-        slug = urlsplit(url).path[1:]
-        return slug, os.path.join(ARTICLES_DIR, '{}{}'.format(slug, ARTICLES_EXT))
+def get_slug(header):
+    # Get unique, user-friendly slug for url.
     slug = re.sub('[^\w ]+', '', header).strip().lower()
     slug = re.sub('[ ]+', '-', slug)
     slug = '{}_1'.format(
-                    ''.join([SLUG_CHARS[a] if a in SLUG_CHARS else a for a in slug])
+                    ''.join([SLUG_TRANS_MAP[a] if a in SLUG_TRANS_MAP else a for a in slug])
                     )[:MAX_SLUG_LENGTH]
-    filename = '{}{}'.format(slug, ARTICLES_EXT)
-    filepath = os.path.join(ARTICLES_DIR, filename)
-    while os.path.exists(filepath):
-        filename_base, filename_num = filename[:-len(ARTICLES_EXT)].split('_')
+    filename = '{}{}'.format(slug, articles_ext)
+    while os.path.exists(os.path.join(articles_dir, filename)):
+        filename_base, filename_num = filename[:-len(articles_ext)].split('_')
         slug = '{}_{}'.format(filename_base, int(filename_num) + 1)
-        filename = '{}{}'.format(slug, ARTICLES_EXT)
-        filepath = os.path.join(ARTICLES_DIR, filename)
-    return slug, filepath
+        filename = '{}{}'.format(slug, articles_ext)
+    return slug
 
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # TODO: sould implement good WYSIWYG editor
-    if request.method != 'POST':
+    # TODO: implementation WYSIWYG editor and JS fields checking.
+    if request.method == 'GET':
         return render_template('index.html')
-    header = request.form['header']
-    signature = request.form['signature']
-    body = request.form['body']
-    url = request.form['url']
-    if header == '' or signature == '' or body == '':
+    data = request.form.to_dict()
+
+    # Check for empty header.
+    # TODO: implementation JS fields checking.
+    if data['header'] == '':
         return render_template(
                     'index.html',
-                    header=header,
-                    signature=signature,
-                    body=body,
-                    error=ERRORS['empty_field'])
-    slug, filepath = get_slug_and_filepath(header, url)
-    with open(filepath, encoding='utf-8', mode='w') as file:
-        file.write(json.dumps((header, signature, body), ensure_ascii=False))
+                    **data,
+                    errors=[ERRORS['empty_header']])
+
+    slug = get_slug(data['header'])
     resp = make_response(redirect(slug, code=301))
-    resp.set_cookie(slug, sign(slug), max_age=COOKIE_AGE)
+
+    # Cookie auth process.
+    user_id = request.cookies.get('user_id')
+    if user_id is None:
+        user_id = str(uuid.uuid1())
+        resp.set_cookie('user_id', user_id, max_age=COOKIE_AGE)
+    data['user_id'] = user_id
+
+    # Write article to file.
+    filepath = os.path.join(articles_dir, '{}{}'.format(slug, articles_ext))
+    with open(filepath, encoding='utf-8', mode='w') as file:
+        file.write(json.dumps(data, ensure_ascii=False))
+
     return resp
 
 
-@app.route('/<slug>', methods=['GET'])
+@app.route('/<slug>', methods=['GET', 'POST'])
 def article(slug):
-    with open(os.path.join(ARTICLES_DIR, '{}.txt'.format(slug))) as file:
-        header, signature, body = json.loads(file.read())
-    resp = make_response(
-            render_template(
-                'article.html',
-                 header=header,
-                 signature=signature,
-                 body=body,
-                 url=request.base_url,
-                 edit=verify(slug, request.cookies.get(slug))))
-    return resp
 
+    # Read article from file.
+    filepath = os.path.join(articles_dir, '{}{}'.format(slug, articles_ext))
+    with open(filepath) as file:
+        data = json.loads(file.read())
 
-@app.route('/edit', methods=['GET', 'POST'])
-def edit():
-    if request.method != 'POST':
-        resp = make_response(redirect('/', code=301))
-        return resp
-    header = request.form['header']
-    signature = request.form['signature']
-    body = request.form['body']
-    url = request.form['url']
-    resp = make_response(
-            render_template(
-                'edit.html',
-                **request.form))
+    # Cookie auth process.
+    user_id = request.cookies.get('user_id')
+    editable = data['user_id'] == user_id
+
+    # Url handle logic.
+    if request.method == 'GET':
+        resp = make_response(render_template(
+                                'article.html',
+                                **data,
+                                editable=editable))
+
+    elif request.form['process'] == 'edit':
+        resp = make_response(render_template(
+                                'edit.html',
+                                **data))
+
+    elif request.form['process'] == 'save':
+        data = request.form.to_dict()
+        del(data['process'])
+        data['user_id'] = user_id
+        with open(filepath, encoding='utf-8', mode='w') as file:
+            file.write(json.dumps(data, ensure_ascii=False))
+        resp = make_response(render_template(
+                                'article.html',
+                                **data,
+                                editable=editable))
+
     return resp
 
 
 if __name__ == "__main__":
     load_dotenv()
+    host = os.environ.get('HOST')
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    articles_dir = os.environ.get('ARTICLES_DIR')
+    articles_ext = os.environ.get('ARTICLES_EXT')
+    app.run(host=host, port=port)
